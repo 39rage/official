f6017bb83c48c6a8cd2f20f1c825398b5c556d59
39rage <39rage@gmail.com>
2026-05-17 21:54:44 +0900

    Update music-data.js

--- qurage/music-data.js
+++ qurage/music-data.js
@@ -8,14 +8,28 @@ const allAlbums = [
         desc: "初めて作った5曲がまとまってます。<br>デモ曲のようなミニアルバム。",
         booth: "https://casbgcasbg.booth.pm/items/2691899",
         tracksCount: "5 Tracks"
+    },
+    {
+        id: "deep-upbeat", 
+        title: "Deep Upbeat",
+        subtitle: "1st Single",
+        img: "https://fromtheasia.com/wp-content/uploads/NCG93-1.jpg",
+        desc: "ひたすらに眠たい曲を作りました。<br>おまけはちょっとオシャレにしたバージョンです。",
+        booth: "https://casbgcasbg.booth.pm/items/2691947",
+        tracksCount: "2 Tracks"
     }
 ];
 
 // 【全曲のリスト】
 const allTracks = [
+    // First step 収録曲
     { title: "British Brixton", file: "2020102923 British_Brixton-100_C.mp3", albumId: "first-step" },
     { title: "Accelerate Afghanistan", file: "2020103012 Accelerate_Afghanistan_TH-102_Cm.mp3", albumId: "first-step" },
     { title: "AABS 80s", file: "2020103014 AABS_80s-128_Cm.mp3", albumId: "first-step" },
     { title: "Altered Afterburner AYB", file: "2020103018 Altered_Afterburner_AYB-129_Csm.mp3", albumId: "first-step" },
-    { title: "Alternating synth layers", file: "2020103111 Alternating_synth_layers-125_Dm.mp3", albumId: "first-step" }
+    { title: "Alternating synth layers", file: "2020103111 Alternating_synth_layers-125_Dm.mp3", albumId: "first-step" },
+    
+    // Deep Upbeat 収録曲
+    { title: "Deep Upbeat EP", file: "2020110114 Deep_Upbeat_EP-60_D.mp3", albumId: "deep-upbeat" },
+    { title: "Deep Upbeat Dense Alert", file: "2020110114_2 Deep_Upbeat_Dense_Alert-60_D.mp3", albumId: "deep-upbeat" }
 ];
